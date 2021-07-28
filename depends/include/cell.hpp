7e4497a9b825a97a3fbbbfa2d43fa08c2645794b
Nemausa <tappanmorris@outlook.com>
2021-07-28 16:50:28 +0800

    Fix Windows compile error

--- depends/include/cell.hpp
+++ depends/include/cell.hpp
@@ -18,7 +18,10 @@
     #define FD_SETSIZE      65535
     #define WIN32_LEAN_AND_MEAN
     #define _WINSOCK_DEPRECATED_NO_WARNINGS
+    #include <windows.h>
     #include <winsock2.h>
+    #pragma comment(lib, "ws2_32.lig")
+    #include <mswsock.h>
 #else
 #ifdef __APPLE__
     #define _DARWIN_UNLIMITED_SELECT
