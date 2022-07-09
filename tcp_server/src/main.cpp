b135f11cc5fef8c33b7c0b950cfcdcb28e9c6820
limeixiong <limx@car-ocean.com>
2022-07-09 17:37:33 +0800

    添加glog日志库

--- tcp_server/src/main.cpp
+++ tcp_server/src/main.cpp
@@ -1,5 +1,6 @@
 #include <thread>
 #include <cstring>
+#include "glog/logging.h"
 
 #include "depends/tcp_mgr.hpp"
 #include "utils/conf.hpp"
@@ -75,6 +76,20 @@ private:
 };
 
 int main(int argc, char *args[]) {
+
+    // 初始化参数
+    FLAGS_logtostderr = false;  //TRUE:标准输出,FALSE:文件输出
+    FLAGS_alsologtostderr = true;  //除了日志文件之外是否需要标准输出
+    FLAGS_colorlogtostderr = true;  //标准输出带颜色
+    FLAGS_logbufsecs = 0;   //设置可以缓冲日志的最大秒数，0指实时输出
+    FLAGS_max_log_size = 10;  //日志文件大小(单位：MB)
+    FLAGS_stop_logging_if_full_disk = true; //磁盘满时是否记录到磁盘
+    google::InitGoogleLogging("mqttserver");
+    google::SetLogDestination(google::GLOG_INFO,"./test");
+    LOG(INFO) << "this is log";
+    LOG(WARNING) << "this is warnning";
+    LOG(ERROR) << "this is error";
+    google::ShutdownGoogleLogging();
 
     config::instance().load("server.conf");
     const char *ip = config::instance().get_string("ip");
