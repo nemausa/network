53884c8982ac60a204c1eadb6ce48fafa0d3f14f
Nemausa <tappanmorris@outlook.com>
2022-08-24 15:40:22 +0800

    vscode 环境

--- launch/osx_tcp_server.sh
+++
@@ -1,3 +0,0 @@
-ulimit -n 10240
-export LD_LIBRARY_PATH=./lib/osx:$LD_LIBRARY_PATH
-./bin/tcp_server
--- launch/unix_tcp_client.sh
+++
@@ -1,3 +0,0 @@
-ulimit -n 102400
-export LD_LIBRARY_PATH=./lib/unix:$LD_LIBRARY_PATH
-./bin/tcp_client
