017f640a2f4ff195e45924503fdfa4b6a0c28a00
Nemausa <tappanmorris@outlook.com>
2023-05-25 14:52:45 +0000

    Add glog
    Signed-off-by: Nemausa <tappanmorris@outlook.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <glog/logging.h>
+
 using namespace std;
 
-int main()
-{
+int main() {
+    // Initialize Google's logging library.
+    google::InitGoogleLogging("Google.txt");
+    FLAGS_log_dir = "./log";  
+    LOG(INFO) << "Found " << 5 << " cooikes";
+    google::ShutdownGoogleLogging();
     cout << "hello world" << endl;
     getchar();
 }
